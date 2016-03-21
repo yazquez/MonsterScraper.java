b9dc254ec25150330dce384cfed2f2931187c8b3
yazquez <yazquez@gmail.com>
2016-03-21 17:43:48 +0100

    Integrate with MySql (use it to save results)

--- src/main/java/com/yazquez/monsterScraper/data/JdbcDataManager.java
+++ src/main/java/com/yazquez/monsterScraper/data/JdbcDataManager.java
@@ -1,21 +1,68 @@
 package com.yazquez.monsterScraper.data;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import com.yazquez.monsterScraper.entities.SearchEntity;
 
 public class JdbcDataManager implements DataManager {
 
-	@Override
-	public List<SearchEntity> getSearchs() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    List<SearchEntity> searchs = SearchConfigurationManager.getSearchs();
+    private DataSource dataSource;
 
-	@Override
-	public void saveResults() {
-		// TODO Auto-generated method stub
-		
-	}
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public List<SearchEntity> getSearchs() {
+        // TODO Auto-generated method stub
+        return searchs;
+    }
+
+    @Override
+    public void saveResults() {
+        String sql = "insert into results(date, country, city, technology, occurences) values (?,?,?,?,?)";
+        Connection conn = null;
+        try {
+            conn = dataSource.getConnection();
+            PreparedStatement ps = conn.prepareStatement(sql);
+            for (SearchEntity search : this.getSearchs()) {
+                Date date = new SimpleDateFormat("yyyy/MM/dd").parse(search.getDate());
+                ps.setDate(1, new java.sql.Date(date.getTime()));
+                ps.setString(2, search.getCountry());
+                ps.setString(3, search.getCity());
+                ps.setString(4, search.getTechnology());
+                ps.setInt(5, search.getResult());
+
+                ps.executeUpdate();
+            }
+
+            ps.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException();
+                }
+            }
+        }
+    }
 
 }
